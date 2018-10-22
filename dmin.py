c4f671de51e918c39cd859a63f6d00eb36dce539
Yadouken <yass@DESKTOP-50DCS1G.localdomain>
2018-10-22 11:18:17 +0200

    Yassine : l'algorithme de distance est prêt Brian : Il reste a creer la méthode qui retourne la liste des représentants rangés dans l'ordre des classes

---
+++ dmin.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
+
+train_data = loadmat('train_32x32.mat')
+test_data = loadmat('test_32x32.mat')
+
+#Images de taille n*n
+n = 32
+
+#Liste des représentants pour chaque classe 
+#representants[i] = vecteur RGB représentant la classe i
+representants = []
+
+#Calcul de la distance euclidienne entre deux vecteurs RGB 
+#renvoit le résultat
+def distanceEuclidienne(X,Y):
+	#Flatten : applati le vecteur de taille (32,32,3) 
+	#devient une liste
+	a = X.flatten()
+	b = Y.flatten()
+	res = np.dot(a,b)
+	return res
+
+#Input : image dont on veut connaitre la classe
+#Distance euclidienne avec chaque représentants de classe
+#Retourne la liste des distances rangés dans l'ordre
+#L[i] = distance entre image et representant classe i (i de 0 à 9) 
+def LDistances(image):
+	L = []
+	for i in range(len(representants)):
+		resultat = distanceEuclidienne(image, representants[i])
+		L.append(resultat)
+	return L
+
+def DeterminerClasse(image):
+	#Récupération des distances entre chaque représentant
+	L = LDistances(image)
+	return L.index(min(L))
--- projet.py
+++ projet.py
@@ -5,14 +5,43 @@ import matplotlib.pyplot as plt
 train_data = loadmat('train_32x32.mat')
 test_data = loadmat('test_32x32.mat')
 
+#Images de taille n*n
+n = 32
 
-
-for i in range (20):
+"""for i in range (20):
 	plt.imshow(train_data['X'][:, :, :, i])
 	plt.show()
 
 
 def changeContraste(index):
-	image = train_data['X'][:,:,:,index]
-	
+	compteur = 0
+	#pour chaque couleur
+	for i in range (3):
+		#pour chaque ligne
+		for j in range(n):
+			#pour chaque colonne
+			for k in range(n):
+				compteur+=1
+				if(train_data['X'][:,:,i,index][j][k]<30):
+					train_data['X'][:,:,i,index][j][k]=0
+			
+					
+	print(compteur)		
+	plt.imshow(train_data['X'][:,:,i,index])
+	plt.show()
 
+def Contraste(image):
+	image[image>155] = 255
+	image[image<20] = 0
+
+	plt.imshow(image[:,:,:,3])
+	plt.show()
+
+#Contraste(train_data['X'])
+
+
+resultat = np.sqrt(((train_data['X'][:,:,:,0] - train_data['X'][:,:,:,1])**2).sum(-1))
+print(resultat)
+"""
+resultat = (train_data['X'][:,:,:,0]).flatten()
+print(len(resultat))
