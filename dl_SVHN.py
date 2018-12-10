98dab3c4e697b7d6eb5089a3e84bd4cdca7d7cd3
Yadouken <yass@DESKTOP-50DCS1G.localdomain>
2018-12-10 08:53:03 +0100

    Mise a jour

--- dl_SVHN.py
+++ dl_SVHN.py
@@ -24,15 +24,52 @@ class CNN(nn.Module):
 
             return x
 
+class LeNet(nn.Module):
+
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(x.shape[0], -1) # Flatten the tensor
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.log_softmax(self.fc3(x), dim=1)
+
+        return x
+
+class MLP(nn.Module):
+
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(32*32*3, 32*16*3)
+        self.fc2 = nn.Linear(32*16*3, 16*16*3)
+        self.fc3 = nn.Linear(16*16*3, 200)
+        self.fc4 = nn.Linear(200, 10)
+
+    def forward(self, x):
+        x = x.contiguous().view(x.shape[0], -1) # Flatten the tensor
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.log_softmax(self.fc4(x), dim=1)
+
+        return x
 
 if __name__ == '__main__':
 
     # Load the dataset
-    train_data = loadmat('train_32x32.mat')
-    test_data = loadmat('test_32x32.mat')
+    train_data = loadmat('train_32x32_pretraitement.mat')
+    test_data = loadmat('test_32x32_pretraitement.mat')
 
-    trainsize = 73257
-    testsize = 26032
+    trainsize = 5000
+    testsize = 1000
 
     train_label = train_data['y'][:trainsize]
     train_label = np.where(train_label==10, 0, train_label)
--- svm.py
+++ svm.py
@@ -7,8 +7,8 @@ from sklearn.metrics import classification_report, confusion_matrix
 train_data = loadmat('train_32x32.mat')
 test_data = loadmat('test_32x32.mat')
 
-lentrain = 73257
-lentest = 5000
+lentrain = 10000
+lentest = 2000
 
 X_train = [[] for x in range(lentrain)]
 X_test =  [[] for y in range(lentest)]
@@ -27,7 +27,7 @@ for u in range(lentrain):
 for v in range(lentest):
 	Y_test[v] = test_data['y'][v][0]
 
-svclassifier = SVC()  
+svclassifier = SVC(C=0.9, cache_size=200, class_weight=None, coef0 =0.0, decision_function_shape='ovo', gamma=0.2,kernel="poly", max_iter=-1, probability = False, random_state = None, shrinking=True, tol=0.001,verbose = False)  
 
 print("Entrainement")
 svclassifier.fit(X_train, Y_train) 
@@ -35,12 +35,8 @@ svclassifier.fit(X_train, Y_train)
 print("Prediction")
 y_pred = svclassifier.predict(X_test)
 
-for i in range (10) :
-	print("Image " +str(i)+":")
-	print(str(Y_test[i])+" "+str(y_pred[i])+"\n")
-
-#accuracy = svclassifier.score(X_test, Y_test)
-#print("Accuracy : ", accuracy*100)
+accuracy = svclassifier.score(X_test, Y_test)
+print("Accuracy : ", accuracy*100)
 
 # print("Resultats")
 #print(confusion_matrix(Y_test,y_pred))  
